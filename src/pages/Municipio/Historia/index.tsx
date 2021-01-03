3914c0d3c1e9372a0ac78c18f0b4b2b8040ac11e
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2021-01-02 21:44:06 -0500

    finish sobre a historia page

--- src/pages/Municipio/Historia/index.tsx
+++ src/pages/Municipio/Historia/index.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import styles from './styles';
+import FastImage from 'react-native-fast-image'
+import {
+    View,
+    ScrollView,
+    Text,
+    TouchableOpacity
+} from 'react-native';
+//@ts-ignore
+import GoiaLogo from '../../../assets/Goia.png'
 const Historia: React.FC = () => {
+
+    const handleWebView = () =>{
+
+    }
     return (
         <>
+            <ScrollView style={{backgroundColor:'#FFFFFF'}} showsVerticalScrollIndicator={false}>
+                <View style={styles.container}>
+                    <FastImage source={GoiaLogo} resizeMode={FastImage.resizeMode.contain} style={styles.banner} />
+                    <Text style={styles.text}>Goiana é um município brasileiro do estado de Pernambuco, Região Nordeste do país. Encontra-se localizado no extremo norte da Região Metropolitana do Recife, fazendo divisa com a Região Metropolitana de João Pessoa. Está situado no litoral, a 62 km do Recife, 51 km da capital paraibana e 2 187 km de Brasília. Sua população estimada em 2019 era de 79 758 habitantes e a sede municipal se situa a treze metros de altitude.</Text>
+                    <View style={styles.separate} />
+                    <TouchableOpacity onPress={handleWebView} activeOpacity={0.7} style={styles.mais}>
+                        <Text style={styles.textSub}>Ver Mais</Text>
+                    </TouchableOpacity>
+                    <View style={styles.separate} />
+  
+                </View>
+                <View style={{...styles.separate, height: 70}} />
+            </ScrollView>
         </>
     )
 }
